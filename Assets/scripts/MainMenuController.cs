66b2c163a5eddd305d7c3c212ab645af6b6d5ab1
Sebastian Song <sebastianbsong@gmail.com>
2016-03-30 18:47:14 -0700

    implemented basic splash screens and menu

---
+++ Assets/scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+	
+	public void OnStart() {
+		Debug.Log ("Start Button pressed");
+		SceneManager.LoadScene (2);
+	}
+
+	public void OnHiScores() {
+		Debug.Log ("HiScores Button pressed");
+	}
+
+	public void OnOptions() {
+		Debug.Log ("Options Button pressed");
+	}
+
+	public void OnQuit() {
+		Debug.Log ("Quit Button pressed");
+		Application.Quit ();
+	}
+}
---
+++ Assets/scripts/SplashScreenController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashScreenController : MonoBehaviour {
+
+	public float delay;
+
+	IEnumerator Start () {
+		yield return new WaitForSeconds(delay);
+
+		SceneManager.LoadScene (1);
+	}
+}
