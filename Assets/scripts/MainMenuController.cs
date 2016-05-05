a60b2d9eff98d1d656e403765e41d0da94bb7d5f
Sebastian Song <sebastianbsong@gmail.com>
2016-05-04 20:05:15 -0700

    playable

--- Assets/scripts/MainMenuController.cs
+++ Assets/scripts/MainMenuController.cs
@@ -6,20 +6,10 @@ using System.Collections;
 public class MainMenuController : MonoBehaviour {
 	
 	public void OnStart() {
-		Debug.Log ("Start Button pressed");
 		SceneManager.LoadScene (3);
 	}
 
-	public void OnHiScores() {
-		Debug.Log ("HiScores Button pressed");
-	}
-
-	public void OnOptions() {
-		Debug.Log ("Options Button pressed");
-	}
-
 	public void OnQuit() {
-		Debug.Log ("Quit Button pressed");
 		Application.Quit ();
 	}
 }
