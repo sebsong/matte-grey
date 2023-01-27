a905ff76680c4734b02b9d4f7a8b3dd525781c81
Sebastian Song <sebsong@berkeley.edu>
2023-01-26 16:10:22 -0800

    add min 1 second to transition pages

--- Assets/scripts/TransitionController.cs
+++ Assets/scripts/TransitionController.cs
@@ -11,17 +11,21 @@ public class TransitionController : MonoBehaviour {
 	private float blinkCooldown;
 	private bool active;
 
+    private float minSeconds;
+
 
 	// Use this for initialization
 	void Start () {
 		transitionText = GameObject.FindWithTag ("transitionText");
 		blinkCooldown = blinkDelay;
 		active = true;
+        minSeconds = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.anyKey) {
+        minSeconds -= Time.deltaTime;
+		if (minSeconds < 0 && Input.anyKey) {
 			Utils.LoadNextScene ();
 		} else if (blinkCooldown <= 0 || blinkCooldown == blinkDelay) {
 			transitionText.SetActive (active);
